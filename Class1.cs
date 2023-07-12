e5ff030ce57a213a9c640aa3834fdf4e64f623ce
NotTheGrimAngel <101980821+NotTheGrimAngel@users.noreply.github.com>
2023-07-11 22:40:43 -0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace Jhague.KnifetoSword
+{
+    [BepInPlugin(myGUID, pluginName, versionString)]
+    public class KnifeDamagePlugin_BZ : BaseUnityPlugin
+    {
+        private const string myGUID = "com.Jhague.KnifeToSword";
+        private const string pluginName = "Knife Damage Mod BZ";
+        private const string versionString = "1.0.2";
+        public static ConfigEntry<float> ConfigKnifeDamageMultiplier;
+        public static ConfigEntry<float> ConfigKnifeRangeMultiplier;
+        public static ConfigEntry<float> ConfigKnifeWidthMultiplier;
+        private static readonly Harmony harmony = new Harmony(myGUID);
+
+        public static ManualLogSource logger;
+
+        private void Awake()
+        {
+            ConfigKnifeDamageMultiplier = Config.Bind("General",        
+                "KnifeDamageMultiplier",                                
+                1.0f,                                                   
+                "Knife damage multiplier.");
+            ConfigKnifeRangeMultiplier = Config.Bind("General",
+                "Knife Range Mulitplier",
+                3.0f,
+                "Knife Range Multipler.");
+            ConfigKnifeWidthMultiplier = Config.Bind("General",
+                "Knife Width Mulitplier",
+                1.0f,
+                "Knife Width Multipler.");
+
+
+            harmony.PatchAll();
+            Logger.LogInfo(pluginName + " " + versionString + " " + "loaded.");
+            logger = Logger;
+        }
+    }
+}
---
+++ Class2.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using UnityEngine;
+
+namespace Jhague.KnifetoSword
+{
+    /// <summary>
+    /// Class to mod the knife
+    /// </summary>
+    public static class KnifeDamageMod_BZ
+    {
+        [HarmonyPatch(typeof(Knife))]
+        public static class Knife_Patch
+        {
+            [HarmonyPatch(nameof(Knife.Start))]
+            [HarmonyPostfix]
+            public static void Start_Postfix(Knife __instance)
+            {
+
+                float damageMultiplier = KnifeDamagePlugin_BZ.ConfigKnifeDamageMultiplier.Value;
+                float rangeMultiplier = KnifeDamagePlugin_BZ.ConfigKnifeRangeMultiplier.Value;
+                // Double the knife damage
+                float knifeDamage = __instance.damage;
+                float newKnifeDamage = knifeDamage * damageMultiplier;
+                __instance.damage = newKnifeDamage;
+                KnifeDamagePlugin_BZ.logger.LogInfo($"Knife damage was: {knifeDamage}," +
+                    $" is now: {newKnifeDamage}");
+                float knifeRange = __instance.attackDist;
+                float newKnifeRange = knifeRange * rangeMultiplier;
+                __instance.attackDist = newKnifeRange;
+                KnifeDamagePlugin_BZ.logger.LogInfo($"Knife Range was: {knifeRange}," + 
+                    $" the range is now: {newKnifeRange}");
+            }
+        }
+        [HarmonyPatch(typeof(PlayerTool))]
+        public static class PlayerTool_Patch
+        {
+            [HarmonyPatch(nameof(PlayerTool.OnDraw))]
+            [HarmonyPostfix]
+            public static void OnDraw_Postfix(PlayerTool __instance)
+            {
+
+                if (__instance is Knife knife)
+                {
+                    float rangeMultiplier = KnifeDamagePlugin_BZ.ConfigKnifeRangeMultiplier.Value;
+                    float widthMultiplier = KnifeDamagePlugin_BZ.ConfigKnifeWidthMultiplier.Value;
+                    knife.transform.localScale = new Vector3(knife.transform.localScale.x * widthMultiplier, 
+                        knife.transform.localScale.y * rangeMultiplier, 
+                        knife.transform.localScale.z);
+                }
+            }
+        }
+    }
+}
